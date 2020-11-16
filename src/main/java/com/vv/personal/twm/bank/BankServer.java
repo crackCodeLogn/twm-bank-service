91d2aa7c6da5ddceeab13d92a3d98d924e8f480c
crackCodeLogN <taekwondo@Dan1>
2020-11-16 14:15:48 +0530

    Bank service to handle all TWM Banking transactions - online.

---
+++ src/main/java/com/vv/personal/twm/bank/BankServer.java
@@ -0,0 +1,21 @@
+package com.vv.personal.twm.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Vivek
+ * @since 16/11/20
+ */
+@EnableSwagger2
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class BankServer {
+    public static void main(String[] args) {
+        SpringApplication.run(BankServer.class, args);
+    }
+}
